3138e65b5fa30d5b9def794c0bee6c69b90a6478
zrr1999 <2742392377@qq.com>
2023-03-14 18:28:34 +0800

    add llvm ir

--- examples/yolov8/main.py
+++ examples/yolov8/main.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 import tvm
-from tvmer.utils import load_onnx, gen_library
+from tvmer.utils import load_onnx, gen_library, infer_time
+from tvm.contrib.graph_executor import GraphModule
 
 
-def main(target=tvm.target.arm_cpu("rk3399"), dtype="int8", lib_path: str = "lib/arm_cpu_rk3399.so"):
+def main(target=tvm.target.arm_cpu(), dtype="int8", lib_path: str = "lib/arm_cpu_default.so"):
     mod, params = load_onnx(path="./model/yolov8s_detect.onnx", batch_size=1, dtype=dtype)
     lib = gen_library(mod, params, target, lib_path)
-    # print(lib.get_source())
-    # print(len(lib.imported_modules))
+    target = target if isinstance(target, str) else target.device_name
+    with open(f".tvmer/llvm_ir/{target}_{dtype}.source", mode="w") as f:
+        f.write(lib.get_lib().get_source())
+
     # print(lib.imported_modules[0].get_source())
     # with tvm.transform.PassContext(opt_level=3):
     #     graph_json, lib, params = relay.build(mod, target=target, params=params)
@@ -25,10 +28,23 @@ def main(target=tvm.target.arm_cpu("rk3399"), dtype="int8", lib_path: str = "lib
 
 
 if __name__ == '__main__':
-    # import tvm.contrib.hexagon
-    # main(tvm.target.hexagon(), "lib/hexagon.so")
-    main("cuda", "float32", "lib/cuda_f32.so")
-    main("cuda", "int8", "lib/cuda_i8.so")
-    main("llvm", "float32", "lib/llvm_f32.so")
-    main("llvm", "int8", "lib/llvm_i8.so")
-    # main(tvm.target.arm_cpu("rk3399"))
+    import numpy as np
+
+    # rk3588_target = tvm.target.Target(
+    #     "llvm -keys=arm_cpu,cpu -device=arm_cpu "
+    #     "-model=rk3588 -mattr=+neon"
+    # )
+
+    rk3588_target = tvm.target.Target(
+        "llvm -keys=arm_cpu,cpu -device=arm_cpu "
+        "-model=rk3588 -mtriple=aarch64-linux-gnu -mattr=+neon"
+    )
+
+    # main("llvm", "int8", "lib/llvm_i8.so")
+    main(rk3588_target, "int8", "lib/arm_rk3588_i8.so")
+    # main(tvm.target.arm_cpu(), "int8", "lib/arm_default_i8.so")
+    print(infer_time("lib/arm_rk3588_i8.so", {"479": np.zeros([1, 64, 8400])}, repeat=1000))
+    # print(infer_time("lib/llvm_i8.so", {"479": np.zeros([1, 64, 8400])}, repeat=1000))
+    # print(infer_time("lib/arm_default_i8.so", {"479": np.zeros([1, 64, 8400])}, repeat=1000))
+    # main("cuda", "float32", "lib/cuda_f32.so")
+    # main("cuda", "int8", "lib/cuda_i8.so")
--- tvmer/utils.py
+++ tvmer/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import tvm
 from tvm import relay, IRModule
+from tvm.contrib.graph_executor import GraphModule
 import onnx
 import onnx_graphsurgeon as gs
 
@@ -22,8 +23,19 @@ def load_onnx(path: str, batch_size: int = 1, dtype: str = "int8") -> tuple[IRMo
 
 def gen_library(mod, params, target="llvm", path: str = ".tvmer/lib/compiled.so"):
     with tvm.transform.PassContext(opt_level=3):
-        graph_json, lib, params = relay.build(mod, target=target, params=params)
+        lib = relay.build(mod, target=target, params=params)
     if not os.path.exists(os.path.split(path)[0]):
         os.makedirs(os.path.split(path)[0])
     lib.export_library(path)
     return lib
+
+
+def infer_time(lib_path: str, input_data, dev=tvm.cpu(), repeat=10):
+    import time
+    print(tvm.runtime.load_module(lib_path))
+    module = GraphModule(tvm.runtime.load_module(lib_path)['default'](dev))
+
+    t = time.time()
+    for _ in range(repeat):
+        module.run(**input_data)
+    return (time.time() - t) / repeat
